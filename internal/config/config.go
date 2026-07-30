94d4273b56737ee6470e65136aa58ddd3b7e59fc
TiyeeJiang <tiyee@live.com>
2026-07-30 22:26:25 +0800

    Add DNSPlatform and CDNPlatform fields to Config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,8 @@ type ICredentials struct {
 }
 type Config struct {
 	Platform        string       `yaml:"platform"`
+	DNSPlatform        string    `yaml:"dns_platform"`
+	CDNPlatform        string    `yaml:"cdn_platform"`
 	CertOnly        bool         `yaml:"cert_only"`
 	Renew           bool         `yaml:"renew"`
 	Domain          string       `yaml:"domain"`
